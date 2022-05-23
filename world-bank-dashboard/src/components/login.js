bec0bd3681dab3d45273c6ac3d48778b08cc1d56
agConvoy <alex.convoy@talktalk.net>
2022-05-23 15:03:11 +0100

    added base login component

---
+++ world-bank-dashboard/src/components/login.js
@@ -0,0 +1,23 @@
+function login() {
+  //add functionality to login form
+  return (
+    <div className="Login">
+      <header className="Login-header">Please Log-in</header>
+      <form className="Login-form">
+        <label for="username">Username</label>
+        <br />
+        <input type="text" id="username">
+          username
+        </input>
+        <br />
+        <label for="password">Password</label>
+        <br />
+        <input type="text" id="password">
+          password
+        </input>
+      </form>
+    </div>
+  );
+}
+
+export default login;
