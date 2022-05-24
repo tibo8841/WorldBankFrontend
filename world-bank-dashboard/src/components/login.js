4e7d08146fc98b266f7cd38126f853acfd0a0c54
aycin1 <aycinkaya98@gmail.com>
2022-05-24 15:45:44 +0100

    Added functionality to user login page as well as some notes on what more needs to be done (i.e. hashing and salting, checking entries against database)

--- world-bank-dashboard/src/components/login.js
+++ world-bank-dashboard/src/components/login.js
@@ -1,23 +1,40 @@
-function login() {
-  //add functionality to login form
+import { useState } from "react";
+
+export default function LoginUser() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  // Add hashing and salting of password for security
+  // Add functionality to check user info against users table
+  // => when user info matches database (goes to search page) and when it doesn't (throws an error and asks to try again)
+
+  function handleSubmit() {
+    return;
+  }
+
   return (
     <div className="Login">
-      <header className="Login-header">Please Log-in</header>
+      <h1 className="Login-header">Welcome to the World Bank Dashboard!</h1>
+      <h3 className="Login-header2">Please log in</h3>
       <form className="Login-form">
-        <label for="username">Username</label>
-        <br />
-        <input type="text" id="username">
-          username
-        </input>
-        <br />
-        <label for="password">Password</label>
-        <br />
-        <input type="text" id="password">
-          password
-        </input>
+        <label for="username">
+          Username
+          <input
+            type="text"
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
+        </label>
+        <label for="password">
+          Password
+          <input
+            type="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
+        </label>
+        <input type="submit" value="Login" onClick={handleSubmit}></input>
       </form>
     </div>
   );
 }
-
-export default login;
