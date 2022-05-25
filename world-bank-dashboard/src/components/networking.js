132215d90a63008b194732d02522867e1029dbdb
tibo8841 <thibaut.hucker@gmail.com>
2022-05-25 17:25:15 +0100

    some fetch requests

--- world-bank-dashboard/src/components/networking.js
+++ world-bank-dashboard/src/components/networking.js
@@ -1,1 +1,39 @@
-// stuff
+const url = "http://localhost/8080";
+
+export async function getSearchHistory(userID) {
+  const userIDObject = { userID: userID };
+  await fetch(`${url}/history`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userIDObject),
+  });
+}
+
+export async function postSearchHistory(
+  userID,
+  countries,
+  indicators,
+  yearRange
+) {
+  const searchHistory = {
+    userID: userID,
+    countries: countries,
+    indicators: indicators,
+    yearRange: yearRange,
+  };
+  await fetch(`${url}/search`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(searchHistory),
+  });
+}
+
+export async function getSearchResults(countries, indicators, yearRange) {
+  await fetch(
+    `${url}/search?country=${countries}&indicator=${indicators}&yearStart=${yearRange[0]}&yearEnd=${yearRange[1]}`
+  );
+}
