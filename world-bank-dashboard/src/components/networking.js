11bd24a3484d2a82c0aaebcb63a971ab19f906a7
tibo8841 <thibaut.hucker@gmail.com>
2022-05-26 17:13:21 +0100

    all fetch requests done

--- world-bank-dashboard/src/components/networking.js
+++ world-bank-dashboard/src/components/networking.js
@@ -1,4 +1,4 @@
-const url = "http://localhost/8080";
+const url = "https://shrouded-plateau-99916.herokuapp.com/";
 
 export async function getSearchHistory(userID) {
   const userIDObject = { userID: userID };
@@ -36,4 +36,34 @@ export async function getSearchResults(countries, indicators, yearRange) {
   await fetch(
     `${url}/search?country=${countries}&indicator=${indicators}&yearStart=${yearRange[0]}&yearEnd=${yearRange[1]}`
   );
+}
+
+export async function getUserLogin(username, password) {
+  const userLogin = { username: username, password: password };
+  await fetch(`${url}/login`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userLogin),
+  });
+}
+
+export async function registerNewUser(
+  username,
+  password,
+  passwordConfirmation
+) {
+  const newUserDetails = {
+    username: username,
+    password: password,
+    passwordConfirmation: passwordConfirmation,
+  };
+  await fetch(`${url}/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newUserDetails),
+  });
 }
