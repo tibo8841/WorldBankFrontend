cdc345772925e452a11be8330e7a55d6b1fc69a4
aycin1 <aycinkaya98@gmail.com>
2022-05-27 15:02:06 +0100

    Material UI done

--- world-bank-dashboard/src/components/RegisterUser.js
+++ world-bank-dashboard/src/components/RegisterUser.js
@@ -3,8 +3,55 @@ import { css, jsx } from "@emotion/react";
 import { React, useState } from "react";
 const url = "http://localhost:3000";
 
-const flexContainer = css({ display: "flex" });
-const flexChild = css({ display: "flex" });
+const flexContainer = css({
+  display: "flex",
+  textAlign: "center",
+  justifyContent: "space-between",
+  flexDirection: "column",
+  paddingTop: "1%",
+  paddingBottom: "1%",
+  height: "50%",
+  width: "80%",
+});
+
+const flexChild = css({
+  flex: "1",
+  paddingTop: "2%",
+  paddingBottom: "2%",
+  paddingLeft: "5%",
+  paddingRight: "5%",
+});
+
+const flexChild2 = css({
+  flex: "1",
+  paddingTop: "1%",
+  paddingBottom: "1%",
+});
+
+const heading = css({
+  color: "#102e51",
+  fontWeight: "bold",
+  fontSize: "150%",
+});
+
+const textFormat = css({
+  color: "#e87499",
+  fontWeight: "bold",
+  paddingRight: "1%",
+});
+
+const inputFields = css({ flex: "1", width: "30%", height: "40%" });
+
+const button = css({
+  flex: "1",
+  paddingTop: "2%",
+  paddingBottom: "2%",
+  paddingLeft: "5%",
+  paddingRight: "5%",
+  fontSize: "120%",
+  color: "#e87827",
+  fontWeight: "bold",
+});
 
 export default function RegisterUser() {
   const [username, setUsername] = useState();
@@ -25,39 +72,52 @@ export default function RegisterUser() {
 
   return (
     <div className="Register" css={flexContainer}>
-      <header className="Register-Header">Register New User</header>
-      <form className="Register-form" onSubmit={handleRegistration}>
-        <label htmlFor="username" css={flexChild}>
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          onChange={(e) => setUsername(e.target.value)}
-        ></input>
-        <label htmlFor="password" css={flexChild}>
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
-        <label htmlFor="Password-repeat" css={flexChild}>
-          Re-enter Password
-        </label>
-        <input
-          type="password"
-          id="Password-repeat"
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-        ></input>
+      <header className="Register-Header" css={heading}>
+        Register New User
+      </header>
+      <div css={flexChild}>
+        <form className="Register-form" onSubmit={handleRegistration}>
+          <div css={flexChild2}>
+            <label htmlFor="username" css={textFormat}>
+              Username
+            </label>
+            <input
+              css={inputFields}
+              type="text"
+              id="username"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+          </div>
+          <div css={flexChild2}>
+            <label htmlFor="password" css={textFormat}>
+              Password
+            </label>
+            <input
+              css={inputFields}
+              type="password"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </div>
+          <div css={flexChild2}>
+            <label htmlFor="Password-repeat" css={textFormat}>
+              Re-enter Password
+            </label>
+            <input
+              css={inputFields}
+              type="password"
+              id="Password-repeat"
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+            ></input>
+          </div>
+        </form>
         <input
           type="submit"
           id="Register_button"
-          value="Register New User"
-          css={flexChild}
+          value="Register!"
+          css={button}
         ></input>
-      </form>
+      </div>
     </div>
   );
 }
